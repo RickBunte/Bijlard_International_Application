9fcdfa50ee539b64dc78ed4e79c4af419203649e
Rick Bunte <rickbunte2000@gmail.com>
2021-01-23 12:08:30 +0100

    Working on Excel reader, added Excel sample data

---
+++ BijlandInternationalApplication/BijlandInternationalApplication/Data and Classes/ExcelReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BijlandInternationalApplication.Data_and_Classes
+{
+    public class ExcelReader
+    {
+        #region Properties
+        public Excel.Application xlApp = new Excel.Application();
+        public Excel.Workbook xlWorkbook;
+        public Excel.Worksheet xlWorksheet;
+        public Excel.Range range;
+        #endregion
+
+        public void InitializeExcelObject()
+        {
+            xlWorkbook = xlApp.Workbooks.Open(@"~\Excel\SampleData.xlsx");
+            xlWorksheet = xlWorkbook.Worksheets.get_Item(1) as Excel.Worksheet;
+        }
+    }
+}
---
+++ BijlandInternationalApplication/BijlandInternationalApplication/Data and Classes/Order.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BijlandInternationalApplication.Data_and_Classes
+{
+    public class Order
+    {
+        #region Properties
+        private int _id { get; set; }
+        public DateTime date { get; set; }
+        public string region { get; set; }
+        public string rep { get; set; }
+        public string item { get; set; }
+        public int units { get; set; }
+        public float unitCost { get; set; }
+        #endregion
+
+        #region Constructor
+        public Order(DateTime date, string region, string rep, string item, int units, float unitCost)
+        {
+            this.date = date;
+            this.region = region;
+            this.rep = rep;
+            this.item = item;
+            this.units = units;
+            this.unitCost = unitCost;
+        }
+        #endregion
+    }
+}
